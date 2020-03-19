c04e197ba21c22c6b4cbe81987f0157d8a4f7b62
AndrewDAntonio <github email address>
2020-03-19 11:39:24 -0400

    90 mins...

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,18 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+
+    def all_likes
+       total = 0
+        self.posts.each do |post|
+            total + post.likes
+        end
+        total
+    end
+
+    def featured_post
+        posts = self.posts
+        posts.likes.max
+    end
 
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,11 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
 
+    def avg_age
+        blogs = self.bloggers.uniq
+        ages = blogs.map{|blogger| blogger.age}
+        red_age = ages.reduce{|sum, age| sum + age}
+        avg = red_age/blogs.length
+    end
 end
